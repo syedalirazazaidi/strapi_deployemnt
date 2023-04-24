b2b13c76bac94756f6b58f71a06d1c4f7fba61b7
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-24 14:31:02 +0500

    change url

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  server: {
+    url: env('SERVER_URL', 'https://strapi-test-dd7596.netlify.app'),
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-});
+})
